fa3247e433dcc3842be9d83eee68f213f75095cd
Abbas <abbas@farz.ai>
2026-03-13 12:16:28 -0500

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,31 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+    50: '#f2f8fd',
+    100: '#e4effa',
+    200: '#c3def4',
+    300: '#8dc4ec',
+    400: '#51a6df',
+    500: '#3c98d7',
+    600: '#3C98d7',
+    700: '#17588d',
+    800: '#174b75',
+    900: '#194061',
+    950: '#102841',
+        }
+      },
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
+      boxShadow: {
+        'brand': '0 1px 3px 0 rgba(60, 152, 215, 0.25), 0 1px 2px -1px rgba(60, 152, 215, 0.15)',
+      }
+    }
+  }
+}
+
+
+
+
